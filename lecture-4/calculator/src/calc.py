25dd42c2b3d9550e850d8bd8ce05ee85ccad5277
CHOIDAVIDYUETAO <s2522063@stu.musashino-u.ac.jp>
2025-12-16 22:39:32 +0900

    Add scientific calculator features

---
+++ lecture-4/calculator/src/calc.py
@@ -0,0 +1,208 @@
+import flet as ft
+import math
+
+class CalcButton(ft.ElevatedButton):
+    def __init__(self, text, button_clicked, expand=1):
+        super().__init__()
+        self.text = text
+        self.expand = expand
+        self.on_click = button_clicked
+        self.data = text
+
+class DigitButton(CalcButton):
+    def __init__(self, text, button_clicked, expand=1):
+        CalcButton.__init__(self, text, button_clicked, expand)
+        self.bgcolor = ft.Colors.WHITE24
+        self.color = ft.Colors.WHITE
+
+class ActionButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.Colors.ORANGE
+        self.color = ft.Colors.WHITE
+
+class ExtraActionButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.Colors.BLUE_GREY_100
+        self.color = ft.Colors.BLACK
+
+class ScienceButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.Colors.PURPLE_400
+        self.color = ft.Colors.WHITE
+
+class CalculatorApp(ft.Container):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+        self.width = 350
+        self.bgcolor = ft.Colors.BLACK
+        self.border_radius = ft.border_radius.all(20)
+        self.padding = 20
+        self.content = ft.Column(
+            controls=[
+                ft.Row(controls=[self.result], alignment=ft.MainAxisAlignment.END),
+                ft.Row(
+                    controls=[
+                        ScienceButton(text="sin", button_clicked=self.button_clicked),
+                        ScienceButton(text="cos", button_clicked=self.button_clicked),
+                        ScienceButton(text="tan", button_clicked=self.button_clicked),
+                        ScienceButton(text="log", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        ExtraActionButton(text="AC", button_clicked=self.button_clicked),
+                        ExtraActionButton(text="+/-", button_clicked=self.button_clicked),
+                        ExtraActionButton(text="%", button_clicked=self.button_clicked),
+                        ScienceButton(text="^", button_clicked=self.button_clicked), 
+                        ActionButton(text="/", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="7", button_clicked=self.button_clicked),
+                        DigitButton(text="8", button_clicked=self.button_clicked),
+                        DigitButton(text="9", button_clicked=self.button_clicked),
+                        ActionButton(text="*", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="4", button_clicked=self.button_clicked),
+                        DigitButton(text="5", button_clicked=self.button_clicked),
+                        DigitButton(text="6", button_clicked=self.button_clicked),
+                        ActionButton(text="-", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="1", button_clicked=self.button_clicked),
+                        DigitButton(text="2", button_clicked=self.button_clicked),
+                        DigitButton(text="3", button_clicked=self.button_clicked),
+                        ActionButton(text="+", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="0", expand=2, button_clicked=self.button_clicked),
+                        DigitButton(text=".", button_clicked=self.button_clicked),
+                        ActionButton(text="=", button_clicked=self.button_clicked),
+                    ]
+                ),
+            ]
+        )
+
+    def button_clicked(self, e):
+        data = e.control.data
+        print(f"Button clicked with data = {data}")
+        
+        if self.result.value == "Error":
+             self.result.value = "0"
+             self.reset()
+
+        if data == "AC":
+            self.result.value = "0"
+            self.reset()
+
+        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if self.result.value == "0" or self.new_operand == True:
+                self.result.value = data
+                self.new_operand = False
+            else:
+                self.result.value = str(self.result.value) + str(data)
+
+        elif data in ("+", "-", "*", "/", "^"):
+            self.result.value = str(self.calculate(self.operand1, float(str(self.result.value)), self.operator))
+            self.operator = data
+            if self.result.value == "Error":
+                self.operand1 = "0"
+            else:
+                self.operand1 = float(self.result.value)
+            self.new_operand = True
+
+        elif data in ("="):
+            self.result.value = str(self.calculate(self.operand1, float(str(self.result.value)), self.operator))
+            self.reset()
+
+        elif data in ("%"):
+            self.result.value = str(float(str(self.result.value)) / 100)
+            self.reset()
+
+        elif data in ("+/-"):
+            if float(str(self.result.value)) > 0:
+                self.result.value = "-" + str(self.result.value)
+            elif float(str(self.result.value)) < 0:
+                self.result.value = str(self.format_number(abs(float(str(self.result.value)))))
+        
+        elif data == "sin":
+            val = float(self.result.value)
+            self.result.value = str(self.format_number(math.sin(val)))
+            self.new_operand = True
+            
+        elif data == "cos":
+            val = float(self.result.value)
+            self.result.value = str(self.format_number(math.cos(val)))
+            self.new_operand = True
+            
+        elif data == "tan":
+            val = float(self.result.value)
+            self.result.value = str(self.format_number(math.tan(val)))
+            self.new_operand = True
+
+        elif data == "log":
+            val = float(self.result.value)
+            if val <= 0:
+                self.result.value = "Error"
+            else:
+                self.result.value = str(self.format_number(math.log(val)))
+            self.new_operand = True
+
+        self.update()
+
+    def format_number(self, num):
+        try:
+            if num % 1 == 0:
+                return int(num)
+            else:
+                return num
+        except:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+        try:
+            if operator == "+":
+                return self.format_number(operand1 + operand2)
+            elif operator == "-":
+                return self.format_number(operand1 - operand2)
+            elif operator == "*":
+                return self.format_number(operand1 * operand2)
+            elif operator == "/":
+                if operand2 == 0:
+                    return "Error"
+                else:
+                    return self.format_number(operand1 / operand2)
+            elif operator == "^":
+                return self.format_number(math.pow(operand1, operand2))
+        except:
+            return "Error"
+            
+        return 0
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
+
+def main(page: ft.Page):
+    page.title = "Scientific Calculator"
+    page.window_width = 400
+    page.window_height = 600
+    calc = CalculatorApp()
+    page.add(calc)
+
+ft.app(main)
